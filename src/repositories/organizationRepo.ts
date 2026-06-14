fbd86a9cffab35b55fa593b55eb6656ee70a607f
Navpreet Singh <navpreetg477@gmail.com>
2026-06-14 18:47:06 -0500

    add organizationRepo

---
+++ src/repositories/organizationRepo.ts
@@ -0,0 +1,26 @@
+import type { Role } from "../interfaces/Employee";
+import initialData from "../data/organization.json";
+
+let members: Role[] = initialData as Role[];
+
+const organizationRepo = {
+  getMembers(): Role[] {
+    return [...members];
+  },
+
+  getRoleByName(role: string): Role | undefined {
+    return members.find((m) => m.role === role);
+  },
+
+  createMember(
+    firstName: string,
+    lastName: string,
+    role: string
+  ): Role[] {
+    const newMember: Role = { firstName, lastName, role };
+    members = [...members, newMember];
+    return organizationRepo.getMembers();
+  },
+};
+
+export default organizationRepo;
